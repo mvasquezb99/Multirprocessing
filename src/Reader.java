324649895d5ac992817e19c075853ab19d5dec84
VillegasMich <manuel.villegas.michel@gmail.com>
2024-08-13 15:16:41 -0500

    Reader updated save into pages

--- src/Reader.java
+++ src/Reader.java
@@ -6,19 +6,63 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Reader {
+
+    static final int PAGE_LIMIT = 2000;
+
     public static void main(String[] args) {
         Instant start = Instant.now();
-        List<String> list = new ArrayList<>();
+        List<List<String>> list = new ArrayList<>();
+        int limit = PAGE_LIMIT; // Each char equals 2 bytes; 4k = 4000 bytes
+        List<String> currentData = new ArrayList<>();
         try {
             File myObj = new File(args[0]);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
-                list.add(myReader.nextLine());
+                String line = myReader.nextLine();
+                if (line.length() <= limit) {
+                    limit -= line.length();
+                    currentData.add(line);
+                } else if (line.length() >= limit) {
+                    while (true) {
+                        if (line.length() >= limit) {
+                            String sub1 = line.substring(0, limit);
+                            currentData.add(sub1);
+                            if (currentData.get(0).length() > 0)
+                                list.add(currentData);
+                            currentData = new ArrayList<>();
+                            line = line.substring(limit);
+                            limit = 2000;
+                        } else {
+                            if (line.length() <= limit) {
+                                limit -= line.length();
+                                currentData.add(line);
+                            } else {
+                                if (currentData.get(0).length() > 0)
+                                    list.add(currentData);
+                                limit = 2000;
+                                currentData = new ArrayList<>();
+                                currentData.add(line);
+                                limit -= line.length();
+                            }
+                            break;
+                        }
+                    }
+                } else {
+                    limit = 2000;
+                    if (currentData.get(0).length() > 0)
+                        list.add(currentData);
+                    currentData = new ArrayList<>();
+                    currentData.add(line);
+                    limit -= line.length();
+                }
+            }
+            if (currentData.get(0).length() > 0) {
+                list.add(currentData);
             }
             myReader.close();
             LocalTime finishTime = java.time.LocalDateTime.now().toLocalTime();
             Instant end = Instant.now();
-            System.out.println("Process with PID: " + ProcessHandle.current().pid() + " File: " + args[0]
+            System.out.println("Process with PID: " + ProcessHandle.current().pid() + "File: " + args[0]
                     + " Finish time: " + finishTime
                     + " Time in process (millis): "
                     + Duration.between(start, end).toMillis() + "\n");
